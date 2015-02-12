641ff4068c566e7573cbddff1c2749d58cc04d3f
m.sakamaki <m.sakamaki@gxp.co.jp>
2015-02-12 15:25:45 +0900

    add favicon

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 
-var port = 8000;
+var port = process.env.PORT || 8000;
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
