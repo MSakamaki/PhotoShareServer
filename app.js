b97d036706ca44ec4f7b0bbad8f2b37f5d85441d
m.sakamaki <m.sakamaki@gxp.co.jp>
2015-02-12 22:24:59 +0900

    photo api

--- app.js
+++ app.js
@@ -2,9 +2,15 @@
 
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
 var server = require('http').createServer(app);
-
+var photoDB = require('./photo_data/index')
 var port = process.env.PORT || 8000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+})); 
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -18,10 +24,43 @@ server.listen(port,process.env.OPENSHIFT_NODEJS_IP || process.env.IP || undefine
   console.log('Express server listening on %d, in %s mode', port, app.get('env'));
 });
 
-
-// api 
-app.get('/api/test', function (req, res) {
-  res.json({"id":"0", "message": "hello express server"});
+// すべてのデータを取得
+app.get('/api/photos', function (req, res) {
+  photoDB.find({})
+  .then(function(photos){
+    var retData =[];
+    photos.forEach(function(v){
+      retData.push({usr: v.name ,img: v.img})
+    });
+    res.json({"photo_data": retData});
+  });
 });
 
+// 自分のデータを取得
+app.get('/api/myphotos/:uname', function (req, res) {
+  photoDB.find({name: req.params.uname})
+  .then(function(photos){
+    var retData =[];
+    photos.forEach(function(v){
+      retData.push({usr: v.name ,img: v.img})
+    });
+    res.json({"photo_data": retData});
+  });
+});
+
+// 写真投稿
+app.post('/api/photos', function(req, res){
+  var data = req.body;
+  photoDB.create({
+      name: data.usr,
+      img: data.img
+    }).catch(console.log);
+  res.json(200)
+})
+
+
+/*******/
 exports = module.exports = app;
+/*******/
+
+
